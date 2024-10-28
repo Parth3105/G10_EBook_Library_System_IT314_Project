7f5250db9a0d591ac76bc7a7f667e06d70ff8441
Ayushi <ayushiiijani@gmail.com>
2024-10-28 20:08:34 +0530

    Linked the LandingPage with the HomePage

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,18 +2,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './LandingPage';
+import HomePage from './HomePage';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<HomePage />} /> {/* New Route for HomePage */}
       </Routes>
     </Router>
   );
 };
 
 export default App;
+
 
 
 
---
+++ Frontend/src/HomePage.js
@@ -0,0 +1,13 @@
+// src/HomePage.js
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>Explore more content here.</p>
+    </div>
+  );
+};
+
+export default HomePage;
