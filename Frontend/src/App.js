2e64e3228eb2cac8137166525867bd5d5ab1c110
Ayushi <ayushiiijani@gmail.com>
2024-11-09 01:47:24 +0530

    Resolved merge conflict in App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,29 +2,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './LandingPage';
-<<<<<<< HEAD
 import HomePage from './HomePage';
-=======
 import SelectionPage from './SelectionPage';
-import EmailVerification from './EmailVerification';
->>>>>>> b8689b8dc93b5fe955baa61d32c20ffc57a5f8ca
+//import EmailVerification from './EmailVerification';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-<<<<<<< HEAD
-        <Route path="/home" element={<HomePage />} /> {/* New Route for HomePage */}
-=======
+        <Route path="/home" element={<HomePage />} />
         <Route path="/SelectionPage" element={<SelectionPage />} />
->>>>>>> b8689b8dc93b5fe955baa61d32c20ffc57a5f8ca
       </Routes>
     </Router>
   );
 };
 
 export default App;
+
 
 
 
